f7f78141aa236d41aa1439afb02699cb6e42cbfc
lzy-inbs <jenningsliu@163.com>
2023-06-12 17:04:44 +0800

    finished p3t2 aggs & joins

--- src/execution/nested_index_join_executor.cpp
+++ src/execution/nested_index_join_executor.cpp
@@ -30,8 +30,9 @@ NestIndexJoinExecutor::NestIndexJoinExecutor(ExecutorContext *exec_ctx,
 void NestIndexJoinExecutor::Init() {
   inner_table_info_ = exec_ctx_->GetCatalog()->GetTable(plan_->GetInnerTableOid());
   inner_index_info_ = exec_ctx_->GetCatalog()->GetIndex(plan_->GetIndexOid());
-  ended_ = outer_matched_ = has_outer_tuple_ = false;
-  inner_matches_.clear();
+  ended_ = has_outer_tuple_ = false;
+  match_list_.clear();
+  match_list_iter_ = match_list_.cend();
   outer_child_->Init();
 }
 
@@ -49,67 +50,61 @@ auto NestIndexJoinExecutor::Next(Tuple *tuple, RID *rid) -> bool {
   if (ended_) {
     return false;
   }
+  assert(inner_index_info_->key_schema_.GetColumnCount() == 1);
+
   const auto join_type = plan_->GetJoinType();
   while (true) {
+    bool new_outer_tuple = !has_outer_tuple_;
     if (!has_outer_tuple_) {
-      outer_matched_ = false;
-      LOG_DEBUG("nexting outer tuple");
+      has_outer_tuple_ = true;
+//      LOG_DEBUG("yielding outer tuple");
       if (!outer_child_->Next(OuterTuple(), rid)) {
+//        LOG_DEBUG("ended");
         ended_ = true;
         return false;
       }
     }
 
-    has_outer_tuple_ = true;
-    // find inner tuple
-    Value outer_value_rst =
+    Value outer_join_value =
         plan_->KeyPredicate()->Evaluate(OuterTuple(), outer_child_->GetOutputSchema());
 
-    BUSTUB_ENSURE(inner_index_info_->key_schema_.GetColumnCount() == 1,
-                  "unimplemented multi index join");
-
-    bool inner_iter_ended = inner_match_idx_ == inner_matches_.size();
-
     bool generate_null_result =
-        // inner join never generates null result
-        join_type != JoinType::INNER &&
         // outer join attrs have null values
-        (outer_value_rst.IsNull()
-         // can't find an inner row to match
-         || (inner_iter_ended && !outer_matched_));
+        (outer_join_value.IsNull()
+         // got a new row, but can match a thing
+         // this step also reset the inner index
+         || (new_outer_tuple && !RescanInnerIndex(outer_join_value)))
+
+        // inner join never generates null result
+        // need to be at the latter to ensure the index rescan
+        && join_type != JoinType::INNER;
 
     if (generate_null_result) {
+      has_outer_tuple_ = false;
       GenerateOutTuple(tuple, join_type, false, tuples_);
+//      LOG_DEBUG("yielding null row");
+      return true;
     }
 
-    // this would only be executed exactly once
-    if (!IterInited()) {
-      Tuple scan_key{std::vector{outer_value_rst}, &inner_index_info_->key_schema_};
-      inner_index_info_->index_->ScanKey(scan_key, &inner_matches_, exec_ctx_->GetTransaction());
-      inner_match_idx_ = 0;
-    }
+    // now this situation only indicates that the current scan is over
+    // and the match_list_ mustn't be empty
+    if (match_list_iter_ == match_list_.cend()) {
+//      LOG_DEBUG("get to the end of the match_list(%lu)", match_list_.size());
 
-    // now already traversed the inner index
-    if (inner_match_idx_ >= inner_matches_.size()) {
-      Tuple scan_key{std::vector{outer_value_rst}, &inner_index_info_->key_schema_};
-      inner_index_info_->index_->ScanKey(scan_key, &inner_matches_, exec_ctx_->GetTransaction());
-      inner_match_idx_ = 0;
-    }
-    // both of the situation would cause a Next in the outer child
-    if (generate_null_result || inner_iter_ended) {
-      atomic_store(&has_outer_tuple_, false);
-      atomic_store(&outer_matched_, false);
-      if (generate_null_result) {
-        return true;
-      }
+      has_outer_tuple_ = false;
       continue;
     }
 
-    inner_table_info_->table_->GetTuple(inner_matches_[inner_match_idx_], InnerTuple(),
+    if (new_outer_tuple) {
+//      LOG_DEBUG("got a match size of %lu", match_list_.size());
+    }
+
+    inner_table_info_->table_->GetTuple(*match_list_iter_, InnerTuple(),
                                         exec_ctx_->GetTransaction());
-    ++inner_match_idx_;
-    LOG_DEBUG("incremented iter %d", inner_match_idx_);
+
     GenerateOutTuple(tuple, join_type, true, tuples_);
+//    LOG_DEBUG("yielding non-null row");
+    ++match_list_iter_;
     return true;
   }
 }
--- src/include/execution/executors/nested_loop_join_executor.h
+++ src/include/execution/executors/nested_loop_join_executor.h
@@ -53,26 +53,9 @@ class NestedLoopJoinExecutor : public AbstractExecutor {
   auto GetOutputSchema() const -> const Schema & override { return plan_->OutputSchema(); };
 
  private:
-  void FreeTuples() {
-    FreeTuple(0);
-    FreeTuple(1);
-  }
-
-  void FreeTuple(size_t idx) { tuples_[idx].~Tuple(); }
-
   auto inline OuterTuple() -> Tuple * { return &tuples_[outer_tuple_index_]; }
 
-  auto FreeOuterTuple() -> bool {
-    FreeTuple(outer_tuple_index_);
-    return true;
-  }
-
   auto inline InnerTuple() -> Tuple * { return &tuples_[outer_tuple_index_ ^ 1]; }
-
-  auto FreeInnerTuple() -> bool {
-    FreeTuple(outer_tuple_index_ ^ 1);
-    return true;
-  }
 
   void GenerateOutTuple(Tuple *tuple, JoinType join_type, bool is_match, const Tuple *tuples);
 
@@ -87,10 +70,9 @@ class NestedLoopJoinExecutor : public AbstractExecutor {
 
   size_t outer_tuple_index_{0};
 
-  const Schema* output_schema_;
+  const Schema *output_schema_;
 
   Tuple tuples_[2];
-
 
   std::atomic_bool ended_{false};
   std::atomic_bool outer_matched_{false};
