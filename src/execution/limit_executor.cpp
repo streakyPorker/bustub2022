5807d587b09663ca9ca4e329d5e5fde7398ab272
lzy-inbs <jenningsliu@163.com>
2023-06-12 20:38:45 +0800

    need to fix topn

--- src/execution/limit_executor.cpp
+++ src/execution/limit_executor.cpp
@@ -16,10 +16,24 @@ namespace bustub {
 
 LimitExecutor::LimitExecutor(ExecutorContext *exec_ctx, const LimitPlanNode *plan,
                              std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {}
 
-void LimitExecutor::Init() { throw NotImplementedException("LimitExecutor is not implemented"); }
+void LimitExecutor::Init() {
+  limit_ = plan_->GetLimit();
+  count_ = 0;
+  child_executor_->Init();
+}
 
-auto LimitExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto LimitExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  if (count_ >= limit_) {
+    return false;
+  }
+  if (!child_executor_->Next(tuple, rid)) {
+    limit_ = 0;
+    return false;
+  }
+  ++count_;
+  return true;
+}
 
 }  // namespace bustub
--- src/include/execution/executors/topn_executor.h
+++ src/include/execution/executors/topn_executor.h
@@ -33,7 +33,8 @@ class TopNExecutor : public AbstractExecutor {
    * @param exec_ctx The executor context
    * @param plan The topn plan to be executed
    */
-  TopNExecutor(ExecutorContext *exec_ctx, const TopNPlanNode *plan, std::unique_ptr<AbstractExecutor> &&child_executor);
+  TopNExecutor(ExecutorContext *exec_ctx, const TopNPlanNode *plan,
+               std::unique_ptr<AbstractExecutor> &&child_executor);
 
   /** Initialize the topn */
   void Init() override;
@@ -52,5 +53,45 @@ class TopNExecutor : public AbstractExecutor {
  private:
   /** The topn plan node to be executed */
   const TopNPlanNode *plan_;
+
+  template <typename _Tp>
+  struct TopNCmp : public std::binary_function<_Tp, _Tp, bool> {
+    explicit TopNCmp(const std::vector<std::pair<OrderByType, AbstractExpressionRef>> *sorters,
+                     const Schema *schema)
+        : sorters_(sorters), schema_(schema) {}
+    TopNCmp(const TopNCmp &other) = default;
+
+    const std::vector<std::pair<OrderByType, AbstractExpressionRef>> *sorters_{nullptr};
+    const Schema *schema_{nullptr};
+
+    auto operator()(const _Tp &__x, const _Tp &__y) const -> bool {
+      for (auto sorter = sorters_->crbegin(); sorter != sorters_->crend(); ++sorter) {
+        const auto &type = sorter->first;
+        const auto &order_by = sorter->second;
+        Value v1 = order_by->Evaluate(&__x.first, *schema_);
+        Value v2 = order_by->Evaluate(&__y.first, *schema_);
+        CmpBool rst =
+            type != OrderByType::DESC ? v1.CompareLessThan(v2) : v1.CompareGreaterThan(v2);
+        if (rst == CmpBool::CmpTrue) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+  };
+
+  using TupleRID = std::pair<Tuple, RID>;
+  using TopNTuplePQ = std::priority_queue<TupleRID, std::vector<TupleRID>, TopNCmp<TupleRID>>;
+
+
+  const TopNCmp<TupleRID> cmp_;
+  std::unique_ptr<AbstractExecutor> child_;
+
+  std::vector<TupleRID> tuple_rids_;
+  std::unique_ptr<TopNTuplePQ> topn_tuple_pq_;
+
+  std::atomic_bool inited_{false};
 };
+template struct TopNExecutor::TopNCmp<std::pair<Tuple, RID>>;
 }  // namespace bustub
